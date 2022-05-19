2438952bc8ccd6799f5ff644972da63277cec41a
Yashodhan Sanjay Chaudhari <68093591+its-me-yasho@users.noreply.github.com>
2022-05-19 16:27:41 +0530

    added script1

---
+++ js/script1.js
@@ -0,0 +1,13 @@
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+document.addEventListener("click", function(e){
+	if(e.target.closest(".nav-item")){
+		toggleNav();
+	}
+});
