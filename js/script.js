6c915c1e544adf1ffb82364823301b8f212f9bff
Yashodhan Sanjay Chaudhari <68093591+its-me-yasho@users.noreply.github.com>
2022-05-19 16:48:22 +0530

    script.js updated

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ function toggleNav(){
     navToggler.classList.toggle("active");
     document.querySelector(".nav").classList.toggle("open");
 }
+document.addEventListener("click", function(e){
+	if(e.target.closest(".nav-item")){
+		toggleNav();
+	}
+});
+
+window.addEventListener("scroll", function(){
+	if(this.pageYOffset > 60){
+		document.querySelector(".header").classList.add("sticky");
+
+ 	}
+	else{
+		document.querySelector(".header").classList.remove("sticky");
+	}
+});
