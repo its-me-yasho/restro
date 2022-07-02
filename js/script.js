ee9e827eb23451e522e42c25a35563f003b4a866
its-me-yasho <yashodhanchaudhari2017@gmail.com>
2022-07-02 12:35:53 +0530

    last update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", toggleNav);
-
+	
 function toggleNav(){
     navToggler.classList.toggle("active");
     document.querySelector(".nav").classList.toggle("open");
